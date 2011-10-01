b67def40c920eea061c7441bab9984ae7ec01741
Christopher Wood <caw4567@gmail.com>
2011-10-01 13:11:57 -0400

    Added initial test files for Converter subsystem

---
+++ TuxGuitar-converter/test/TGConverterTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+import org.herac.tuxguitar.app.tools.custom.converter.*;
+
+public class TGConverterTest extends TestCase {
+
+	public TGConverterTest() {
+		
+	}
+	
+	public void testCheckIfFileExists() {
+		// make a file, and then assert that it actually exists!
+	}
+}
